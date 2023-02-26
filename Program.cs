8fdd0f134b52de80fcc95f4f40af8636ee17130d
Nick Shipilov <msunick@mail.ru>
2023-02-26 19:03:25 +0300

    dz1

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿//#1
+
+//Console.WriteLine("write first digit");
+//int num1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("write second digit");
+//int num2 = Convert.ToInt32(Console.ReadLine());
+
+//if (num1 > num2)
+//{
+ //    Console.WriteLine($"first number {num1} is is bigger than second number {num2}");
+//}
+//else
+//{
+ //   Console.WriteLine($"second number {num2} is is bigger than first number {num1}");
+//}
+
+//#2
+// Console.WriteLine("write first digit");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("write second digit");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("wright third digit");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+
+// int max1 = 0;
+
+// if (num1 > num2)
+// {
+//      max1 = num1;
+// }
+// else
+// {
+//     max1 = num2;
+// }
+
+// if (max1 > num3)
+// {
+//     Console.WriteLine($"bigger {max1}");
+// }
+// else 
+// {
+//     Console.WriteLine($"bigger {num3}");
+// }
+
+
+//#3
+//  Console.WriteLine("write digit");
+//  int num1 = Convert.ToInt32(Console.ReadLine());
+//  if (num1 % 2 == 0)
+//  {
+//     Console.WriteLine($"{num1} is even");
+//  }
+//  else
+//  {
+//     Console.WriteLine($"{num1} is odd");
+//  }
+
+
+//#4
+// Console.WriteLine("write digit");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// int evenNum = 2;
+// while(evenNum <= num1)
+// {
+//     if(evenNum == 2)
+//     {
+//         Console.Write(evenNum);
+//     }
+//     else
+//     {
+//         Console.Write(", " + evenNum);
+//     }
+
+//     evenNum += 2;
+// }
